9e371d3593010a51306e8b25f505a01542b887cf
Niya Ma <bellatrix.mny@gmail.com>
2025-07-25 11:12:49 -0700

    add files

---
+++ variables.tf
@@ -0,0 +1,55 @@
+# Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+# ------------------------------------------------------
+# ----- General
+#-------------------------------------------------------
+variable "tenancy_ocid" {
+  default = ""
+}
+variable "user_ocid" {
+  default = ""
+}
+variable "fingerprint" {
+  default = ""
+}
+variable "private_key_path" {
+  default = ""
+}
+variable "private_key_password" {
+  default = ""
+}
+
+variable "region" {
+  description = "The region where resources are deployed."
+  type        = string
+}
+
+variable "availability_domain" {
+  description = "The availability domain where resources are deployed."
+  type        = optional(string, 1)
+}
+
+variable "module_name" {
+  description = "The module name."
+  type        = string
+  default     = "exadata-cloud-service"
+}
+
+variable "enable_output" {
+  description = "Whether Terraform should enable module output."
+  type        = bool
+  default     = true
+}
+
+variable "compartments_dependency" {
+  description = "A map of objects containing the externally managed compartments this module may depend on. All map objects must have the same type and must contain at least an 'id' attribute of string type set with the compartment OCID."
+  type        = map(any)
+  default     = null
+}
+
+variable "network_dependency" {
+  description = "A map of objects containing the externally managed network resources (e.g., subnets, NSGs) this module may depend on. All map objects must have the same type and must contain at least an 'id' attribute of string type set with the resource OCID."
+  type        = map(any)
+  default     = null
+}
