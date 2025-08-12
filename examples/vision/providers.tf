a7db6b57487a8a3e2ccdb73e71e022ddf090b2d2
Niya Ma <niya.m.ma@oracle.com>
2025-08-12 21:57:25 +0000

    Lanz 3882

--- examples/vision/providers.tf
+++ examples/vision/providers.tf
@@ -1,2 +1,21 @@
 # Copyright (c) 2025, Oracle and/or its affiliates. All rights reserved.
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+
+
+provider "oci" {
+  region               = var.region
+  tenancy_ocid         = var.tenancy_ocid
+  user_ocid            = var.user_ocid
+  fingerprint          = var.fingerprint
+  private_key_path     = var.private_key_path
+  private_key_password = var.private_key_password
+}
+
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+    }
+  }
+}
